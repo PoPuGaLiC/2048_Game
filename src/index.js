32c398a622eeb40e650e7c31112d99c92a6d1773
PoPuGaLiC <64127778+PoPuGaLiC@users.noreply.github.com>
2023-05-30 16:05:36 +0500

    Bugfix2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ var ctx = canvas.getContext("2d");
 import {GameScene} from './GameScene';
 import {ASSETS} from './assets';
 
+const COCOS_CONFIG ={
+    "debugMode"     : 1,
+    "showFPS": false,
+    "frameRate"     : 60,
+    "id"            : "canvas",
+    "renderMode"    : 1,
+    // "jsList": ["./src/index.js",
+    // "./cocos2d-js-v3.13/cocos2d-js-v3.13.js",
+    // "./src/GameScene.js",
+    // "./src/BackgroundLayer.js",
+    // "./src/TileLayer.js",
+    // "./src/CountLabel.js",
+    // "./src/Tile.js"]
+}
+
+
 window.onload = function(){
     cc.game.onStart = function(){
         cc.director.setDisplayStats(false);
@@ -11,5 +27,5 @@ window.onload = function(){
         }, this);
         
     };
-    cc.game.run("canvas");
+    cc.game.run(COCOS_CONFIG);
 };
