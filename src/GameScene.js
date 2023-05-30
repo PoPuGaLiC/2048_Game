8795a3fc6e7642207ddb1e97ecde4993cc00e784
PoPuGaLiC <64127778+PoPuGaLiC@users.noreply.github.com>
2023-05-30 21:02:25 +0500

    а

---
+++ src/GameScene.js
@@ -0,0 +1,19 @@
+// var BackgroundLayer = require("./BackgroundLayer");
+// var TileLayer = require("./TileLayer");
+
+import {BackgroundLayer} from './BackgroundLayer';
+import {TileLayer} from './TileLayer';
+
+export class GameScene extends cc.Scene{
+    onEnter(){
+        super.onEnter();
+        let size = cc.director.getWinSize();
+        this.backgroundLayer = new BackgroundLayer();
+        this.addChild(this.backgroundLayer, 1);
+        
+        this.tileLayer = new TileLayer(size);
+        this.tileLayer.setPosition(25, 11);
+        this.addChild(this.tileLayer, 1);
+    }
+    
+};
